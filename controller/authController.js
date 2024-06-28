44991ed1ba49bf137ea52fcd341061b804b298a5
sarath2905 <bsarath2905@gmail.com>
2024-06-28 15:00:59 +0530

    App server

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,35 @@
+const { response } = require("express");
 const user=require("../model/authModel");
 
 //Register user
 
-exports.registerUser =async(req,res)=>{};
+exports.registerUser =async(req,res)=>{
+    try{
+        //req.body
+        const{email,password,name,gender,profilepicture}=req.body;
+
+        // check if user already exists
+        const existingUser=await user.findOne({email})
+        if(existingUser){
+            return res.status(400).json({
+                message:"server error",
+                success:false,
+                statusCode:0,
+            });
+        }
+        
+        
+
+    }catch(err){
+        res.status(500).json({
+            message:"server error",
+            success:false,
+            statusCode:0,
+        });
+
+    };
+
+}
 
 
 //Login User
--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express=require("express");
-const authRoutes=require('./routes/authRoutes');
+const dotenv=require("dotenv");
+const morgan=require("morgan")
+const cors=require("cors");
+const mongoose=require("mongoose")
 
+//routes
+const authRoutes=require("./routes/authRoutes")
+
+dotenv.config();
+const app=express();
+
+//databse connection
+mongoose.connect(process.env.MONGO_URI,{
+    dbName:process.env.DB_NAME,
+}).then(()=>console.log("database connected"))
+.catch((err)=>console.log("db connection failed",err));
+
+
+
+
+//Middleware
 app.use(express.json());
 app.use("/api",authRoutes);
+app.use(cors());
 
+app.use("/api",authRoutes);
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
